3ca0ac1ab577cfc575fb9e95abb7bd9614109732
hantang820 <hantang820@gmail.com>
2023-12-23 20:53:01 +0900

    test

--- AboutMe/settings.py
+++ AboutMe/settings.py
@@ -14,21 +14,25 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kjav#yqv7r58vio6ya0t9d*ru^77v$d)qpngb*-4s0ka_m2#&g'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
